5a151b8fc03fac619c4a6fc821db390f1193dc63
priyanka-1592 <priyanka1592p@gmail.com>
2024-11-23 17:03:52 +0530

    Add initial files

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+   navMenu.classList.add('show-menu');
+  });
+}
+
+if(navClose) {
+  navClose.addEventListener('click', () => {
+   navMenu.classList.remove('show-menu');
+  });
+}
+
+
+
+
+
+
+function imgGallary() {
+ const mainImg = document.querySelector('.details__img'),
+ smallImg = document.querySelectorAll('.details__small-img');
+
+ smallImg.forEach((img) => {
+  img.addEventListener('click', function (){
+    mainImg.src = this.src;
+  });
+ });
+}
+
+imgGallary();
+var swiperProducts = new Swiper('.new__container',{
+    spaceBetween: 14,
+    loop: true,
+
+    navigation:{
+      nextE1: 'swiper-button-next',
+      prevE1: 'swiper-button-prev',
+    },
+
+    breakpoints: {
+      350: {
+        slidesPreView: 2,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 24,
+        },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+        },
+      },
+});
+
+
+var swiperCategories = new Swiper('.categories__container',{
+  spaceBetween: 14,
+  loop: true,
+
+  navigation:{
+    nextE1: 'swiper-button-next',
+    prevE1: 'swiper-button-prev',
+  },
+
+  breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+      },
+    },
+});
+
+const tabs= document.querySelectorAll('[data-target]');
+  tabContents = document.querySelectorAll('[content]');
+
+  tabs.forEach((tab) =>{
+    tab.addEventListener('click',() => {
+      const target = document.querySelector(tab.dataset.target);
+      tabContents.forEach((tabContent) => {
+         tabContent.classList.remove('active-tab');
+      });
+
+      target.classList.add('active-tab');
+      tabs.forEach((tab) =>{
+        tab.classList.remove('active-tab');
+      });
+
+      tab.classList.add('active-tab');
+      });
+
+    });
+  
+    
